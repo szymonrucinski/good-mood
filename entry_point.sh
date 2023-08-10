19548867760cbaba2758a6979a13c1e73ba40dda
Szymon Rucinski <szymonrucinski@icloud.com>
2023-08-10 14:16:56 +0200

    Removing code/files 🔥

--- entry_point.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'Conda disown'
-nohup conda run -n ml python3 app.py &
-
-echo 'Running Jupyter disown'
-conda run -n ml jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token='' --NotebookApp.password=''
